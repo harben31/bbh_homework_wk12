c2cfbaa3e740b13da89dfbacc8622673d93efd36
Benjamin Harris <harben31@gmail.com>
2021-04-19 15:58:41 -0700

    Added more deffinition to tables in schema

--- schema.sql
+++ schema.sql
@@ -16,6 +16,7 @@ CREATE TABLE mgmt(
     last_name VARCHAR(30),
     dept_id INT,
     role_id INT,
+    salary INT,
     PRIMARY KEY (id)
 );
 
@@ -27,13 +28,32 @@ CREATE TABLE roles(
     PRIMARY KEY (id)
 );
 
-CREATE TABLE emp-info(
+CREATE TABLE emp_info(
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     dept_id INT, 
     manager_id INT, 
     role_id INT,
-    salary INT
+    salary INT,
     PRIMARY KEY (id)
-)
+);
+
+
+INSERT INTO dept (dept_name, manager_id)
+VALUES ("accounting", 1),  ("engineering", 2), ("legal", 3);
+
+INSERT INTO mgmt (first_name, last_name, dept_id, role_id, salary)
+VALUES ("joe", "acct", 1, 1, 200000), ("bob", "engin", 2, 2, 300000), ("mary", "law", 3, 3, 400000);
+
+INSERT INTO roles (title, dept_id, manager_id)
+VALUES ("Lead Acct", 1, null),("Lead Engineer", 2, null), ("Lead Lawer", 3, null), 
+("Jr Acct", 1, 1), ("Jr Engineer", 2, 2), ("Jr Lawer", 3, 3);
+
+INSERT INTO emp_info (first_name, last_name, role_id, manager_id, dept_id, salary)
+VALUES ("jane", "jrAcct", 4, 1, 1, 100000), ("fergus", "jrEng", 5, 2, 2, 120000), ("ginny", "jrLaw", 6, 3, 3, 130000);
+
+SELECT * FROM mgmt;
+SELECT * FROM dept;
+SELECT * FROM roles;
+SELECT * FROM emp_info; 
