0512feab59c5554b571de57c0902067518d66ef6
Benjamin Harris <harben31@gmail.com>
2021-04-20 14:44:58 -0700

    view by manager is working with user picking choices popped by db. want to add view by all

--- schema.sql
+++ schema.sql
@@ -10,21 +10,12 @@ CREATE TABLE dept(
     PRIMARY KEY (id) 
 );
 
-CREATE TABLE mgmt(
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    dept_id INT,
-    role_id INT,
-    salary INT,
-    PRIMARY KEY (id)
-);
-
 CREATE TABLE roles(
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     dept_id INT,
     manager_id INT,
+	salary DECIMAL,
     PRIMARY KEY (id)
 );
 
@@ -35,7 +26,6 @@ CREATE TABLE emp_info(
     dept_id INT, 
     manager_id INT, 
     role_id INT,
-    salary INT,
     PRIMARY KEY (id)
 );
 
@@ -43,17 +33,67 @@ CREATE TABLE emp_info(
 INSERT INTO dept (dept_name, manager_id)
 VALUES ("accounting", 1),  ("engineering", 2), ("legal", 3);
 
-INSERT INTO mgmt (first_name, last_name, dept_id, role_id, salary)
-VALUES ("joe", "acct", 1, 1, 200000), ("bob", "engin", 2, 2, 300000), ("mary", "law", 3, 3, 400000);
+INSERT INTO roles (title, dept_id, manager_id, salary)
+VALUES ("Lead Acct", 1, null, 111000),("Lead Engineer", 2, null, 222000), ("Lead Lawer", 3, null, 333000), 
+("Jr Acct", 1, 1, 11000), ("Jr Engineer", 2, 2, 22000), ("Jr Lawer", 3, 3, 33000);
 
-INSERT INTO roles (title, dept_id, manager_id)
-VALUES ("Lead Acct", 1, null),("Lead Engineer", 2, null), ("Lead Lawer", 3, null), 
-("Jr Acct", 1, 1), ("Jr Engineer", 2, 2), ("Jr Lawer", 3, 3);
+INSERT INTO emp_info (first_name, last_name, role_id, manager_id, dept_id)
+VALUES ("joe", "acct", 1, null, 1), ("bob", "engin", 2, null, 2), ("mary", "law", 3, null, 3), ("jane", "jrAcct", 4, 1, 1), ("fergus", "jrEng", 5, 2, 2), ("ginny", "jrLaw", 6, 3, 3);
 
-INSERT INTO emp_info (first_name, last_name, role_id, manager_id, dept_id, salary)
-VALUES ("jane", "jrAcct", 4, 1, 1, 100000), ("fergus", "jrEng", 5, 2, 2, 120000), ("ginny", "jrLaw", 6, 3, 3, 130000);
+INSERT INTO emp_info (first_name, last_name, role_id, manager_id, dept_id)
+VALUES ("Bilbo", "accBag", 4, 1, 1),("Sam", "accGam", 4, 1, 1),("Frodo", "engBag", 5, 2, 2),
+("Pipin", "engTook", 5, 2, 2),("Bob", "lawBlah", 6, 3, 3),("Dick", "lawWolf", 6, 3, 3);
 
-SELECT * FROM mgmt;
 SELECT * FROM dept;
 SELECT * FROM roles;
 SELECT * FROM emp_info; 
+
+SELECT * FROM emp_info WHERE manager_id IS NULL;
+
+SELECT e.id, e.first_name, e.last_name, r.title, r.salary, d.dept_name 
+FROM emp_info AS e 
+INNER JOIN dept AS d 
+	ON e.dept_id = d.id
+INNER JOIN roles AS r
+	ON d.id = r.dept_id;
+
+	
+SELECT emp.first_name, emp.last_name, r.title, r.salary, d.dept_name, concat(mgr.first_name,' ', mgr.last_name) AS manager  
+FROM emp_info emp
+LEFT JOIN emp_info  mgr
+	ON emp.manager_id = mgr.id
+INNER JOIN roles r
+	ON emp.role_id = r.id
+INNER JOIN dept d
+	ON emp.dept_id = d.id;
+	
+
+SELECT emp.first_name, emp.last_name, r.title, r.salary, d.dept_name, concat(mgr.first_name,' ', mgr.last_name) AS manager  
+FROM emp_info emp
+LEFT JOIN emp_info  mgr
+	ON emp.manager_id = mgr.id
+INNER JOIN roles r
+	ON emp.role_id = r.id
+INNER JOIN dept d
+	ON emp.dept_id = d.id
+WHERE emp.manager_id IS NULL;
+
+
+
+SELECT d.dept_name, concat(mgr.first_name,' ', mgr.last_name) AS dept_manager, concat(emp.first_name, emp.last_name) AS dept_employees  
+FROM emp_info emp
+LEFT JOIN emp_info  mgr
+	ON emp.manager_id = mgr.id
+INNER JOIN dept d
+	ON emp.dept_id = d.id
+WHERE d.dept_name = 'legal';
+
+SELECT concat(mgr.first_name,' ', mgr.last_name) AS manager, d.dept_name, concat(emp.first_name, ' ', emp.last_name, r.title) AS subordinates   
+FROM emp_info emp
+RIGHT JOIN emp_info  mgr
+	ON emp.manager_id = mgr.id
+INNER JOIN roles r
+	ON emp.role_id = r.id
+INNER JOIN dept d
+	ON emp.dept_id = d.id
+WHERE mgr.id = 1;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+const cTable = require('console.table')
 
 const Employee = require('./lib/employee');
 const Role = require('./lib/role')
@@ -13,6 +14,43 @@ const db = mysql.createConnection({
     database: 'employees_DB'
 });
 
+let baseQuery = `SELECT ?  
+FROM emp_info emp
+LEFT JOIN emp_info  mgr
+	ON emp.manager_id = mgr.id
+INNER JOIN roles r
+	ON emp.role_id = r.id
+INNER JOIN dept d
+	ON emp.dept_id = d.id;`
+
+const allEmpQuery = `emp.first_name, emp.last_name, r.title, r.salary, d.dept_name, concat(mgr.first_name,' ', mgr.last_name) AS manager`
+
+const allEmpTotQuery = `SELECT emp.first_name, emp.last_name, r.title, r.salary, d.dept_name, concat(mgr.first_name,' ', mgr.last_name) AS manager  
+FROM emp_info emp
+LEFT JOIN emp_info  mgr
+	ON emp.manager_id = mgr.id
+INNER JOIN roles r
+	ON emp.role_id = r.id
+INNER JOIN dept d
+	ON emp.dept_id = d.id;`
+
+const mgrTotQuery = `SELECT concat(mgr.first_name,' ', mgr.last_name) AS manager, d.dept_name, concat(emp.first_name, ' ', emp.last_name, ' ', r.title) AS subordinates   
+FROM emp_info emp
+RIGHT JOIN emp_info  mgr
+	ON emp.manager_id = mgr.id
+INNER JOIN roles r
+	ON emp.role_id = r.id
+INNER JOIN dept d
+	ON emp.dept_id = d.id
+WHERE mgr.id = ?;`
+
+const deptTotQuery = `SELECT d.dept_name, concat(mgr.first_name,' ', mgr.last_name) AS dept_manager, concat(emp.first_name, emp.last_name) AS dept_employees  
+FROM emp_info emp
+LEFT JOIN emp_info  mgr
+	ON emp.manager_id = mgr.id
+INNER JOIN dept d
+	ON emp.dept_id = d.id
+ORDER BY d.id;`
 
 const start = ()=>{
     inquirer.prompt([
@@ -42,31 +80,82 @@ const viewRoster = () => {
                 'by_employee',
                 'by_dept',
                 'by_role',
-                'by_manager'
+                'by_manager',
+                'by_all_employees'
             ]
         }
     ]).then((data) => {
         let cat;
         if(data.viewBy==='by_manager'){
-            cat = 'mgmt'
-        } else if(data.view==='by_dept'){
-            cat = 'dept'
+            viewMgr()
+            cat = mgrTotQuery;
+        } else if(data.viewBy==='by_dept'){
+            // viewDept()
+            cat = deptTotQuery;
         } else if(data.viewBy==='by_role'){
-            cat = 'roles'
+            //viewRoles();
+            cat = 'roles';
+        } else if(data.viewBy==='by_employee'){
+            // viewEmp()
+            cat = 'emp_info WHERE manager_id'
         } else {
-            cat = 'emp_info'
+            //viewAllEmp()
+            cat = allEmpTotQuery;
         }
-        viewBy(cat)
+        // viewBy(cat);
     })
     .catch((err)=>{if(err) throw err})
 }
 
+const viewMgr = () => {
+    db.query(`SELECT * FROM emp_info WHERE manager_id IS NULL`, 
+    (err, res)=>{
+        if(err)throw err;
+        inquirer
+            .prompt([
+                {
+                    type: 'rawlist',
+                    name: 'mgrFullName',
+                    message: 'Select manager to view',
+                    choices(){
+                        const nameArr = []; //add view all mgr 
+
+                        res.forEach(({ first_name, last_name })=>{
+                            nameArr.push(first_name+' '+last_name)
+                        })
+                        return nameArr
+                    }
+                }
+            ])
+            .then((data)=>{
+                console.log(data)
+                res.forEach((emp_info) =>{
+                    const dbQuery = emp_info.first_name.toLowerCase()+ ' '+emp_info.last_name.toLowerCase();
+                    const inqAns = data.mgrFullName.toLowerCase()
+                    if(dbQuery===inqAns){
+                        db.query(mgrTotQuery, 
+                            emp_info.id,
+                            (err, res)=>{
+                                if(err) throw err;
+                                console.table(res);
+                            })
+            
+                    }
+                })
+            })
+        });
+        // db.end(); ends fn before query can fire
+}
+
 const viewBy = (cat) => {
-    db.query(`SELECT * FROM ${cat}`,
+    db.query(cat,
+        
     (err, res)=>{
         if(err)throw err;
         console.log(res);
+        console.table(res);
     });
+    db.end();
 };
 
 const editRoster = () => {
